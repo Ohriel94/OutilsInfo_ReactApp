086ccde71f0c2b4573bf93c017b482fb2407a432
Ohriel94 <brendb@outlook.com>
2023-04-06 16:53:52 -0400

    [test] progres sur appareilsDM.test.js
     PASS  domain/Appareils/appareilsDM.test.js
      recupererAppareils
        √ should call appareilsDB 1 time (3 ms)
        √ should contain correct number of elements (1 ms)
        √ should return the correct elements from DB (1 ms)
      creerAppareils
        √ should call appareilsDB 1 time (60 ms)
        √ should have been called with the right parameters (17 ms)

--- backend/domain/Appareils/appareilsDM.test.js
+++ backend/domain/Appareils/appareilsDM.test.js
@@ -59,65 +59,109 @@ describe('recupererAppareils', () => {
 });
 
 describe('creerAppareils', () => {
- const expected = {
-  type: 'Ordinateur',
-  serialNumber: 9991,
-  etatDisponible: true,
-  details: {
-   marque: 'Asus',
-   modele: 'Alpha',
-   dateAcquisition: '01/01/2000',
-   dateAnnonce: '01/01/2000',
-   dateSortie: '01/01/2000',
-   configuration: {
-    os: 'Windows 10 64x',
-    cpu: 'intel core i7-1165G7 @ 2.80Ghz',
-    gpu: 'Prototype HK7880 64GB',
-    memoire: 16,
-    stockages: [256, 2000, 2000],
+ const expected = [
+  {
+   type: 'Ordinateur',
+   serialNumber: 9991,
+   etatDisponible: true,
+   details: {
+    marque: 'Asus',
+    modele: 'Alpha',
+    dateAcquisition: '01/01/2000',
+    dateAnnonce: '01/01/2000',
+    dateSortie: '01/01/2000',
+    configuration: {
+     os: 'Windows 10 64x',
+     cpu: 'intel core i7-1165G7 @ 2.80Ghz',
+     gpu: 'Prototype HK7880 64GB',
+     memoire: 16,
+     stockages: [256, 2000, 2000],
+    },
+    notes: '',
+    piecesJointes: {},
    },
-   notes: '',
-   piecesJointes: {},
   },
- };
+  {
+   type: 'Ordinateur',
+   serialNumber: 9992,
+   etatDisponible: true,
+   details: {
+    marque: 'Asus',
+    modele: 'Alpha',
+    dateAcquisition: '01/01/2000',
+    dateAnnonce: '01/01/2000',
+    dateSortie: '01/01/2000',
+    configuration: {
+     os: 'Windows 10 64x',
+     cpu: 'intel core i7-1165G7 @ 2.80Ghz',
+     gpu: 'Prototype HK7880 64GB',
+     memoire: 16,
+     stockages: [256, 2000, 2000],
+    },
+    notes: '',
+    piecesJointes: {},
+   },
+  },
+  {
+   type: 'Ordinateur',
+   serialNumber: 9993,
+   etatDisponible: true,
+   details: {
+    marque: 'Asus',
+    modele: 'Alpha',
+    dateAcquisition: '01/01/2000',
+    dateAnnonce: '01/01/2000',
+    dateSortie: '01/01/2000',
+    configuration: {
+     os: 'Windows 10 64x',
+     cpu: 'intel core i7-1165G7 @ 2.80Ghz',
+     gpu: 'Prototype HK7880 64GB',
+     memoire: 16,
+     stockages: [256, 2000, 2000],
+    },
+    notes: '',
+    piecesJointes: {},
+   },
+  },
+ ];
 
  it('should call appareilsDB 1 time', async () => {
   await appareilsDM.creerAppareils(
    1,
-   expected.type,
-   expected.serialNumber,
-   expected.details.marque,
-   expected.details.modele,
-   expected.details.dateAcquisition,
-   expected.details.dateAnnonce,
-   expected.details.dateSortie,
-   expected.details.configuration.os,
-   expected.details.configuration.cpu,
-   expected.details.configuration.gpu,
-   expected.details.configuration.memoire,
-   expected.details.configuration.stockages,
-   expected.details.notes
+   expected[0].type,
+   expected[0].serialNumber,
+   expected[0].details.marque,
+   expected[0].details.modele,
+   expected[0].details.dateAcquisition,
+   expected[0].details.dateAnnonce,
+   expected[0].details.dateSortie,
+   expected[0].details.configuration.os,
+   expected[0].details.configuration.cpu,
+   expected[0].details.configuration.gpu,
+   expected[0].details.configuration.memoire,
+   expected[0].details.configuration.stockages,
+   expected[0].details.notes
   );
   expect(appareilsDB.addMany).toHaveBeenCalledTimes(1);
  });
 
  it('should have been called with the right parameters', async () => {
   await appareilsDM.creerAppareils(
-   1,
-   expected.type,
-   expected.serialNumber,
-   expected.details.marque,
-   expected.details.modele,
-   expected.details.dateAcquisition,
-   expected.details.dateAnnonce,
-   expected.details.dateSortie,
-   expected.details.configuration.os,
-   expected.details.configuration.cpu,
-   expected.details.configuration.gpu,
-   expected.details.configuration.memoire,
-   expected.details.configuration.stockages,
-   expected.details.notes
+   3,
+   expected[0].type,
+   expected[0].serialNumber,
+   expected[0].details.marque,
+   expected[0].details.modele,
+   expected[0].details.dateAcquisition,
+   expected[0].details.dateAnnonce,
+   expected[0].details.dateSortie,
+   expected[0].details.configuration.os,
+   expected[0].details.configuration.cpu,
+   expected[0].details.configuration.gpu,
+   expected[0].details.configuration.memoire,
+   expected[0].details.configuration.stockages,
+   expected[0].details.notes
   );
-  expect(appareilsDB.addMany).toHaveBeenCalledWith([expected]);
+  expect(appareilsDB.addMany).toHaveBeenCalledWith(expected);
  });
 });

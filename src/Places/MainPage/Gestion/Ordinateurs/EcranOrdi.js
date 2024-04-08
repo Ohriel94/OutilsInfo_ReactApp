4f9fd83b3218b2612b56b4b44a3accac6bd8d9e0
Ohriel94 <brendb@outlook.com>
2024-04-08 09:27:14 -0400

    update

--- src/Places/MainPage/Gestion/Ordinateurs/EcranOrdi.js
+++ src/Places/MainPage/Gestion/Ordinateurs/EcranOrdi.js
@@ -9,43 +9,43 @@ import Button from '@mui/material/Button';
 import OrdinateurAccordeon from '../../../../Components/Ordinateurs/OrdinateurAccordeon';
 
 const EcranOrdinateurs = (props) => {
- const navigate = useNavigate();
- const [ordinateurs, setOrdinateurs] = React.useState([]);
- const { token, notifier } = props;
+	const navigate = useNavigate();
+	const [ordinateurs, setOrdinateurs] = React.useState([]);
+	const { token, notifier } = props;
 
- const componentStyle = {
-  style: {
-   background: '0971f1',
-  },
-  sx: { padding: 1, marginBottom: 0.5, zIndex: 1 },
- };
+	const componentStyle = {
+		style: {
+			background: '0971f1',
+		},
+		sx: { padding: 1, marginBottom: 0.5, zIndex: 1 },
+	};
 
- const getOrdinateursRequest = Axios.get('http://localhost:3001/ordinateurs');
+	const getOrdinateursRequest = Axios.get('http://localhost:3001/ordinateurs');
 
- React.useEffect(() => {
-  notifier.asyncBlock(getOrdinateursRequest, (resp) => {
-   setOrdinateurs(resp.data);
-  });
- }, []);
+	React.useEffect(() => {
+		notifier.asyncBlock(getOrdinateursRequest, (resp) => {
+			setOrdinateurs(resp.data);
+		});
+	}, []);
 
- return (
-  <React.Fragment>
-   <Box sx={componentStyle.sx}>
-    <Button
-     onClick={() => navigate('/gestion/ordinateurs/ajouter')}
-     variant='contained'
-     color='primary'
-     size='small'>
-     Ajouter
-    </Button>
-   </Box>
-   <Box sx={{ marginTop: 2 }}>
-    {ordinateurs.map((ordinateur, ordinateurKey) => (
-     <OrdinateurAccordeon ordinateur={ordinateur} key={ordinateurKey} notifier={notifier} />
-    ))}
-   </Box>
-  </React.Fragment>
- );
+	return (
+		<React.Fragment>
+			<Box sx={componentStyle.sx}>
+				<Button
+					onClick={() => navigate('/gestion/ordinateurs/ajouter')}
+					variant='contained'
+					color='primary'
+					size='small'>
+					Ajouter
+				</Button>
+			</Box>
+			<Box sx={{ marginTop: 2 }}>
+				{ordinateurs.map((ordinateur, ordinateurKey) => (
+					<OrdinateurAccordeon ordinateur={ordinateur} key={ordinateurKey} notifier={notifier} />
+				))}
+			</Box>
+		</React.Fragment>
+	);
 };
 
 export default EcranOrdinateurs;
